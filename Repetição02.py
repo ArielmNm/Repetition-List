72bf8386cd35c4590e7c1223d56c65fed57c1b57
unknown <arielmmnogueira@gmail.com>
2023-04-14 14:32:55 -0300

    2.Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.

--- Repetição02.py
+++ Repetição02.py
@@ -5,11 +5,7 @@ print("Senha Usuário:")
 SenhaUsuario = input()
 
 while nomeUsuario == SenhaUsuario:
-    print("Acesso Permitido")
+    print("Acesso Negado")
     print(input(nomeUsuario))
 else:
-    print("Senha Incorreta")
-    print("Nome Usuário:")
-    nomeUsuario = input()
-    print("Senha Usuário:")
-    SenhaUsuario = input()
+    print("Acesso Permitido:", nomeUsuario)
